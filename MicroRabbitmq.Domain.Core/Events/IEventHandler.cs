d38d9a713497402eabc469d4c258dbd4a36cb42e
Tinashe Zulu <tinzulu@gmail.com>
2020-08-07 19:43:57 +0200

    Implemented interfaces for domain.core

---
+++ MicroRabbitmq.Domain.Core/Events/IEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbitmq.Domain.Core.Events
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
