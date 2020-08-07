670c8320dc6ec47a21e94cc96544ff5f12a8811c
Tinashe Zulu <tinzulu@gmail.com>
2020-08-07 22:21:33 +0200

    Initial commit of Infrastructor dependency project

---
+++ MicroRabbitmq.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbitmq.Domain.Core.Bus;
+using MicroRabbitmq.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbitmq.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
